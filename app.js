37d94b2009c6ee839e6acdd57c084cd54e212185
Abdul Samad <samadjs100@gmail.com>
2024-08-19 00:14:03 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,153 @@
+import { 
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  sendPasswordResetEmail,
+  fetchSignInMethodsForEmail }
+  from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
+import { app } from "./firebase.js";
+
+const auth = getAuth(app);
+
+const signUpUser = async (ev) => {
+  ev.preventDefault();
+  try {
+    const email = document.querySelector("#suemail");
+    const pass = document.querySelector("#supass");
+    if (email.value == "") {
+      let txt = document.querySelector("#emptyemail");
+      txt.innerText = "Kindly Enter Email";
+      setTimeout(() => {
+        txt.innerText = "";
+      }, 2000);
+      return;
+    }
+    if (pass.value == "") {
+      let txt = document.querySelector("#emptypass");
+      txt.innerText = "Kindly Enter Password";
+      setTimeout(() => {
+        txt.innerText = "";
+      }, 2000);
+      return;
+    }
+    await createUserWithEmailAndPassword(auth, email.value, pass.value);
+    alert("Signup Succesful");
+  }
+  catch (e) {
+    console.log(e);
+  }
+}
+
+const loginUser = async (ev) => {
+  ev.preventDefault();
+  try {
+    const email = document.querySelector("#email");
+    const pass = document.querySelector("#pass");
+    if (email.value == "") {
+      let txt = document.querySelector("#emptyemaill");
+      txt.innerText = "Kindly Enter Email";
+      setTimeout(() => {
+        txt.innerText = "";
+      }, 2000);
+      return;
+    }
+    if (pass.value == "") {
+      let txt = document.querySelector("#emptypassl");
+      txt.innerText = "Kindly Enter Password";
+      setTimeout(() => {
+        txt.innerText = "";
+      }, 2000);
+      return;
+    }
+    await signInWithEmailAndPassword(auth, email.value, pass.value);
+    window.location.href = "login.html"
+  }
+  catch (e) {
+    const errormessage = e.message
+    console.log(errormessage);
+    if (errormessage == "Firebase: Error (auth/invalid-credential).") {
+      let txt = document.querySelector("#invalid");
+      txt.innerText = "Email/Password is Wrong";
+      setTimeout(() => {
+        txt.innerText = "";
+      }, 2000);
+      return;
+    }
+    if (errormessage == "Firebase: Error (auth/invalid-email).") {
+      let txt = document.querySelector("#invalid");
+      txt.innerText = "Kindly type Correct Email Format";
+      setTimeout(() => {
+        txt.innerText = "";
+      }, 2000);
+      return;
+    }
+  }
+}
+
+const resetpass = async (ev) => {
+  ev.preventDefault();
+  try {
+    const email = document.querySelector("#emailr");
+    if (email.value == "") {
+      let txt = document.querySelector("#emptyemailr");
+      txt.innerText = "Kindly Enter Email";
+      setTimeout(() => {
+        txt.innerText = "";
+      }, 2000);
+      return;
+    }
+    const methods = await fetchSignInMethodsForEmail(auth, email.value);
+    if (methods.length === 0) {
+      let txt = document.querySelector("#emptyemailr");
+      txt.innerText = "Email does not exist";
+      setTimeout(() => {
+        txt.innerText = "";
+      }, 2000);
+      return;
+    }
+
+    await sendPasswordResetEmail(auth, email.value);
+    console.log("Email Sent");
+  } catch (e) {
+    const errormessage = e.message;
+    console.log(errormessage);
+    let txt = document.querySelector("#emptyemailr");
+    if (errormessage == "Firebase: Error (auth/invalid-email).") {
+      txt.innerText = "Kindly type Correct Email Format";
+      setTimeout(() => {
+        txt.innerText = "";
+      }, 2000);
+      return;
+    }
+  }
+};
+
+
+
+document.querySelector("#loginlink").addEventListener("click", () => {
+  document.querySelector("#signupform").style.display = "none";
+  document.querySelector("#loginform").style.display = "block";
+  document.querySelector("#resetdiv").style.display = "none";
+});
+
+document.querySelector("#signuplink").addEventListener("click", () => {
+  document.querySelector("#signupform").style.display = "block";
+  document.querySelector("#loginform").style.display = "none";
+  document.querySelector("#resetdiv").style.display = "none";
+});
+
+document.querySelector("#signuplinkr").addEventListener("click", () => {
+  document.querySelector("#signupform").style.display = "block";
+  document.querySelector("#loginform").style.display = "none";
+  document.querySelector("#resetdiv").style.display = "none";
+});
+
+document.querySelector("#Reset").addEventListener("click", () => {
+  document.querySelector("#signupform").style.display = "none";
+  document.querySelector("#loginform").style.display = "none";
+  document.querySelector("#resetdiv").style.display = "block";
+});
+
+document.querySelector("#signup").addEventListener("click", signUpUser);
+document.querySelector("#login").addEventListener("click", loginUser);
+document.querySelector("#resetpass").addEventListener("click", resetpass);
---
+++ firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBH45S5jFLNT2J6w8ntxihWa5hHRbUD_Sw",
+    authDomain: "my-first-project-4414e.firebaseapp.com",
+    projectId: "my-first-project-4414e",
+    storageBucket: "my-first-project-4414e.appspot.com",
+    messagingSenderId: "338684673100",
+    appId: "1:338684673100:web:e976092959e37eca6288e9",
+    measurementId: "G-HY93KYSCR9"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
