5542a8aae2b94b6305ec7074af0c35d9e94a0c8e
Abdul Samad <50093236+abdulsamad100@users.noreply.github.com>
2024-08-24 14:33:56 +0500

    Update app.js

--- app.js
+++ app.js
@@ -37,24 +37,26 @@ const signUpUser = async (ev) => {
   catch (e) {
     const errormessage = e.message
     console.log(errormessage);
-    if (errormessage == "Firebase: Error (auth/invalid-credential).") {
-      let txt = document.querySelector("#invalid");
-      txt.innerText = "Email/Password is Wrong";
-      setTimeout(() => {
-        txt.innerText = "";
-      }, 2000);
-      return;
-    }
+
     if (errormessage == "Firebase: Error (auth/invalid-email).") {
-      let txt = document.querySelector("#invalid");
+      console.log("error aagaandj");
+      let txt = document.querySelector("#invalid2");
       txt.innerText = "Kindly type Correct Email Format";
       setTimeout(() => {
         txt.innerText = "";
       }, 2000);
       return;
     }
+    if (errormessage == "Firebase: Error (auth/email-already-in-use).") {
+      let txt = document.querySelector("#invalid2");
+      txt.innerText = "Email already in use kindly use Login";
+      setTimeout(() => {
+        txt.innerText = "";
+      }, 2000);
+      return;
+    }
     if (errormessage == "Firebase: Password should be at least 6 characters (auth/weak-password).") {
-      let txt = document.querySelector("#invalid");
+      let txt = document.querySelector("#invalid2");
       txt.innerText = "Email Must be 6 Letter atleast";
       setTimeout(() => {
         txt.innerText = "";
